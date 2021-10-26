6b15a55895d88b55494b663e9240746663944ff0
Nathan-Bauer <77609916+Nathan-Bauer@users.noreply.github.com>
2021-10-26 23:59:21 +0200

    Update index.php and server.mjs

---
+++ src/website/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <title>Spielesammlung</title>
+  <link rel='stylesheet' type='text/css' href='main.css'>
+</head>
+
+<body>
+  <?php include("svg/board-four-players.svg"); ?>
+</body>
+</html>
